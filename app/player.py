cb3c188721f7dcc6314a2ca2f61f03ddf3cd7d5c
lunarzoe <65912429+tbh-luna@users.noreply.github.com>
2025-08-21 00:28:45 +0800

    player.py modified, player test made.

--- app/player.py
+++ app/player.py
@@ -1,3 +1,15 @@
 class Player:
-    def __init__(self, name):
-        pass
+    def __init__(self, id, playerName):
+        self.id = id
+        self.playerName = playerName
+
+    @property
+    def __uid(self):
+        return self.id
+
+    @property
+    def __name(self):
+        return self.playerName
+
+    def __str__(self):
+        print("Player name:", self.__name(), "ID:", self.__uid())
